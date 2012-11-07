e2217dd592fb7310df6044943975f1cb064b5fe9
ashokmiryala <aayush@aayush-ThinkCentre-A58.(none)>
2012-11-07 12:53:32 +0530

    testing commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedmineApp::Application.config.secret_token = 'd3bf9b258e3e4965d67383eb9aa8e8a3100f088784cc056aef37f816551c83f7564789a2a1792d3e9098cbe0de54fef00490ac9408e11027191254af4d0d1759'
