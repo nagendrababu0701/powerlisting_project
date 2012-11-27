db9bd013a58a3feb4c7b371dcf4dc47105b5e339
wavespot <wavespot@wavespot-desktop.(none)>
2012-11-27 15:48:58 +0530

    adding images and implemented yelp and foresquare

--- app/controllers/users_business_locations_controller.rb
+++ app/controllers/users_business_locations_controller.rb
@@ -31,96 +31,65 @@ end
 
 def search_business_details
 user=User.current
+address=""
+address=user.address1 if user
+
 @name=user.firstname
 @login_time=user.last_login_on
 
-@s = Geocoder.search(params[:city],params[:business])
+phone=params[:ph_no].to_s
+city=params[:city].to_s
+business=params[:business].to_s
+state=params[:business_location].to_s
+country=params[:country].to_s
+pincode=params[:pincode].to_s
+match=phone
 
-		client = Yelp::Client.new
-    request = Yelp::Phone::Request::Number.new(
-          :phone_number =>  params[:ph_no],
-          :yws_id => 'GanonVA_293b8gzCHFgHdQ')
-      response = client.search(request)
-    @yelp_results=""
-
-    for i in 0...response["businesses"].length
-        if(response["businesses"][i]["zip"]==params[:pincode])
-        request_results = Yelp::Review::Request::Location.new(
-                    :address => response["businesses"][i]["address1"],
-                    :city => response["businesses"][i]["city"],
-                    :state => response["businesses"][i]["state"],
-                    :term => response["businesses"][i]["name"],
-                    :yws_id => 'GanonVA_293b8gzCHFgHdQ')
-    		response_results = client.search(request_results)
-        @yelp_results=response_results["message"]["text"]
-      	end
-    end
-   
+if !phone.blank? && !city.blank?
+e1=Net::HTTP.get(URI.parse(URI.escape("http://api.yelp.com/phone_search?phone="+phone+"&ywsid=GanonVA_293b8gzCHFgHdQ")))
+search_address=JSON.parse(e1)
+   if(search_address["businesses"][0]["address1"].include?(city) || search_address["businesses"][0]["city"].include?(city) )
+        @response_results=search_address 
+        @yelp_results=@response_results["message"]["text"]
+   end
+elsif !phone.blank?
+e=Net::HTTP.get(URI.parse(URI.escape("http://api.yelp.com/phone_search?phone="+phone+"&ywsid=GanonVA_293b8gzCHFgHdQ")))
+@response_results=JSON.parse(e)
+@yelp_results=@response_results["message"]["text"]
+elsif !city.blank?
+e=Net::HTTP.get(URI.parse(URI.escape("http://api.yelp.com/business_review_search?term="+business+"&location="+address+","+city+","+country+","+pincode+"&ywsid=GanonVA_293b8gzCHFgHdQ")))
+@response_results=JSON.parse(e)
+@yelp_results=@response_results["message"]["text"]
+end
 
 #yls = Yahoo::LocalSearch.new('0yJmk9cUNjYUg0RWhVOG1aJmQ9WVdrOVNUZEdkMjFrTXpJbWNHbzlNVEV4TVRJd05UWXkmcz1jb25zdW1lcnNlY3JldCZ4PWI')
 #@yahoo_results = yls.locate params[:business], params[:pincode], 5
-
+debugger
 foursquare=Foursquare::Base.new('2LRH0UGPXER4KLVDBCOHZUNKKWWCM5JI0B2F05IDFUEREUMD','M4EK4OH2FMKH0WNFJYDPL2GWNAF1AOCTZ4YE1XFE22OQXN0H')
-@venues = foursquare.venues.search(:query => params[:business], :ll => "000,00", :near => params[:city])
-
-     business_location=BusinessLocation.new(:address=> params[:address], :city => params[:city], :state=> params[:state], :ph_no => params[:ph_no], :business_name=>params[:business], :pincode => params[:pincode], :user_id => user.id, :login_time => user.last_login_on)
-     i=1
-     business_user_id=BusinessLocation.find_last_by_user_id(user.id)
-       if(business_user_id)
-           business_location.logincount = business_user_id.logincount.to_i+1
-       else
-           business_location.logincount = i
-       end
-     business_location.save
+@s = Geocoder.search(city+","+state+","+country+","+pincode)
+@venues = foursquare.venues.search(:query => params[:business], :ll => @s[0].latitude.to_s+","+@s[0].longitude.to_s, :intent => :match)
 
 
-directories= Directory.new
-if(@s[0])
- directories.business_location_id=business_location.id
- directories.business="MAPQUEST"
- directories.business_lising_information=@s[0].formatted_address
- directories.categories=params[:business]
-directories.status="Found"
-directories.save
-end
-  
-directories1= Directory.new
+
+business_location=BusinessLocation.new(:address=> params[:address], :city => params[:city], :state=> params[:business_location], :ph_no => params[:ph_no], :business_name=>params[:business], :pincode => params[:pincode], :user_id => user.id, :login_time => user.last_login_on)
+business_location.save
+
+u=UsersBusinessLocation.new
+u.directory_savings(business_location.id,"MAPQUEST",@s[0].formatted_address,"Found")
+
  if(@yelp_results=="OK")
-   directories1.business_location_id=business_location.id
-   directories1.business=response_results["businesses"][0]["name"]
-   directories1.business_lising_information=response["businesses"][i]["city"]+","+response["businesses"][i]["city"]
-   directories1.categories=response_results["businesses"][0]["categories"]
-   directories1.web_site=response_results["businesses"][0]["url"]
-   directories1.photos=response_results["businesses"][0]["photo_url"]
-   directories1.status="Found"
-   directories1.save
+   u.directory_savings(business_location.id,@response_results["businesses"][0]["name"],@response_results["businesses"][0]["city"]+","+@response_results["businesses"][0]["state"],"Found")
 end
 
-directories2= Directory.new
 if(@venues)
   if(@venues["places"])
-    if(@venues["places"][0].location["postalCode"]==params[:pincode])
-     directories2.business_location_id=business_location.id
-     directories2.business="Foursquare"
-     directories2.business_lising_information=@venues["places"][0].location["city"]+","+@venues["places"][0].location["state"]
-     directories2.categories=@venues["places"][0].categories
-     directories2.photos=@venues["places"][0].icon
-     directories2.status="Found"
-    directories2.save
-    end 
-    end 
+   u.directory_savings(business_location.id,"Foursquare",@venues["places"][0].location["city"]+","+@venues["places"][0].location["state"],"Found")
+   end 
 end
 
-directories3= Directory.new
 if(@yahoo_results)
   if(@yahoo_results[0].title)
-     directories3.business_location_id=business_location.id
-     directories3.business="Yahoo"
-     directories3.business_lising_information=params[:city]+","+params[:state]+","+params[:country]
-     directories3.categories=@yahoo_results[0].categories
-     directories3.web_site=@yahoo_results[0].business_url
-    directories3.status="Found"
-    directories3.save
+  u.directory_savings(business_location.id,"Yahoo",params[:city]+","+params[:business_location]+","+params[:country],"Found")
 end
 end
 
@@ -131,4 +100,5 @@ private
 def state_select(object, method, country, options = {}, html_options = {})
     ActionView::Helpers::InstanceTag.new(object, method, self, options.delete(:object)).to_state_select_tag(country, options, html_options)
   end
+
 end
